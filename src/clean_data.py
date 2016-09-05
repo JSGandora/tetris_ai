4f396901351e5035d6d1ac1de0d5af30bd0dd88b
Jason Shi <jason.g.shi@gmail.com>
2016-09-05 09:48:52 +0800

    Add evluate results

---
+++ src/clean_data.py
@@ -0,0 +1,2 @@
+f = open("Tetris Trials.txt", 'r')
+g =
--- src/test.py
+++ src/test.py
@@ -1,10 +1,34 @@
-board = [0] * 10
-board = [board] * 22
-for row in board:
-    print row
+import plotly.plotly as py
+import plotly.graph_objs as go
 
-board[1][2] = 2
-print 'board[1][2] = 2'
+# Create random data with numpy
+import numpy as np
 
-for row in board:
-    print row
+N = 100
+random_x = np.linspace(0, 1, N)
+random_y0 = np.random.randn(N)+5
+random_y1 = np.random.randn(N)
+random_y2 = np.random.randn(N)-5
+
+# Create traces
+trace0 = go.Scatter(
+    x = random_x,
+    y = random_y0,
+    mode = 'markers',
+    name = 'markers'
+)
+trace1 = go.Scatter(
+    x = random_x,
+    y = random_y1,
+    mode = 'lines+markers',
+    name = 'lines+markers'
+)
+trace2 = go.Scatter(
+    x = random_x,
+    y = random_y2,
+    mode = 'lines',
+    name = 'lines'
+)
+
+data = [trace0, trace1, trace2]
+py.plot(data, filename='scatter-mode')
